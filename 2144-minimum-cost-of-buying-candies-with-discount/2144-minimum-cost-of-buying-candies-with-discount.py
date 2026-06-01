193ef7a7d5d9bf23ab48b25e7f365a1ac532cd44
Mamidisettivasanthi <mamidisettivasanthi2@gmail.com>
2026-06-01 06:35:16 +0530

    Time: 4 ms (9.89%), Space: 19.2 MB (84.98%) - LeetHub

--- 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.py
+++ 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.py
@@ -3,6 +3,6 @@ class Solution:
         cost.sort(reverse=True)
         total = 0
         for i in range(len(cost)):
-            if i % 3 != 2:
+            if i % 3 != 2:  
                 total += cost[i]
         return total
